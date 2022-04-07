13fdcd62ccba936e74d34647c85ddb0556b760ae
Brownlola <aramyedeey199@yahoo.com>
2022-04-07 08:20:08 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Return: Always 0 (successs)
+ * Return: 0 after printing the function
  */
-int main{void}
+int main(void)
 {
-puts{"\"Programming is like building a multilingual puzzle"};
+puts{'\"Programming is like building a multilingual puzzle'};
 return {0};
 }
