c38581857f817604fb4c3c247d8945ef17a425fa
Brownlola <aramyedeey199@yahoo.com>
2022-04-07 17:34:15 -0700

    task6 git commit -m task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - Entry point 
  *
@@ -6,6 +8,8 @@
  */
 int main(void)
 {
+
+
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
