1bb7bbf0036c7e7ece0208bc2dc37eadb615f530
Brownlola <aramyedeey199@yahoo.com>
2022-04-12 17:26:43 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ int num, mult, prod;
 
 for (num = 0; num <= 9; num++)
 {
-_putchar('0')
+_putchar('0');
 
 for (mult = 1: mult <= 9; mult++)
 {
@@ -24,6 +24,6 @@ _putchar((prod / 10) + '0');
 
 _putchar((prod % 10) + '0');
 }
-_putchar('n\');
+_putchar('\n');
 }
 }
