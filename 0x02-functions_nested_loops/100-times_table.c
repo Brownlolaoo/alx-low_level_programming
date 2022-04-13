00879558c4e0cc80bf340e242713f62f8ea06fe4
Brownlola <aramyedeey199@yahoo.com>
2022-04-12 22:09:10 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,10 @@ for (num = 0; num <= n; num++)
 {
 _putchar('0');
 
-for (mult = 1: mult <= n; mult++)
+for (mult = 1; mult <= n; mult++)
 {
-_putchar(',')
-_putchar(' ')
+_putchar(',');
+_putchar(' ');
 
 prod = num * mult;
 
@@ -28,7 +28,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
-_putchar(((prod / 10)) % 10 + '0'):
+_putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
