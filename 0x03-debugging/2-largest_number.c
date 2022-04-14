5bbdf45b0d4ea1e283a1b0d1c5ad8c9c5c7c153b
Brownlola <aramyedeey199@yahoo.com>
2022-04-13 21:46:14 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,15 +15,18 @@ int largest;
 if (a >= b && a >= c)
 {
 largest = a;
-{
+}
 else if (b >= a && b >= c)
 {
 largest = b;
 }
-else
+else if(a > b)
 {
 largest = c;
 }
-
+else
+{
+largest = b;
+}
 return (largest);
 }
