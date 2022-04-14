31b1b7401905bfb752a8b329046165141139d1e7
Brownlola <aramyedeey199@yahoo.com>
2022-04-14 14:43:20 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,17 @@
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers
- *
- * Return: Always 0.
+ * print_most_numbers - prints the numbers, from 0 to 9
  */
 void print_most_numbers(void)
 {
-
 int i;
-
-i = 0;
-while (i < 10)
+for (i = 0; i < 10; i++)
 {
-if (i != 2 && i != 4)
+if (i ! = 2 && i != 4)
 {
-_putchar (i + '0');
+_putchar(i + '0');
 }
-i++;
 }
-
-
-_putchar ('\n');
+_putchar('\n');
 }
