3bf22b06a46ef52439dc07f28c5881b8ba863959
Brownlola <aramyedeey199@yahoo.com>
2022-04-15 03:57:52 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,24 +5,24 @@
  *
  * Return: Always 0.
  */
- void print_line(int n)
- {
+void print_line(int n)
+{
 
- int i;
+int i;
 
- if (n > 0)
- {
- for (i = 0; i < n; i++)
- {
- _putchar (95);
- }
+if (n  > 0)
+{
+for (i = 0; i <  n; i++)
+{
+_putchar (95);
+}
 
- _putchar ('\n');
- }
+_putchar ('\n');
+}
 
- else
- {
- _putchar ('\n');
- }
+else
+{
+_putchar ('\n');
+}
 
- }
+}
