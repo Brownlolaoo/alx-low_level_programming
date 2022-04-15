d0b0aed4e287677e4cf1a270c609b568d9250626
Brownlola <aramyedeey199@yahoo.com>
2022-04-14 19:02:08 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,10 @@
 /**
  * print_most_numbers - print numbers
  * Return: Always 0.
- *
  */
-void print_most_number(void)
+void print_most_numbers(void)
 {
+
 int i;
 
 i = 0;
@@ -13,7 +13,7 @@ while (i < 10)
 {
 if (i != 2 && i != 4)
 {
-_putchar (i = '0');
+_putchar (i + '0');
 }
 i++;
 }
