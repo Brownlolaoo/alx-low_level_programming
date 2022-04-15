4152489627622848e08033749a63e60d3ed275cd
Brownlola <aramyedeey199@yahoo.com>
2022-04-15 07:05:51 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,14 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 /**
- * Main - print factors
+ * main - print factors
  *
  * Return: int
  */
+
 int main(void)
 {
 unsigned long n, i, max;
 n = 612852475143;
 max = 1;
+
 for (i = 3; i <= n; i += 2)
 {
 while (n % i == 0)
