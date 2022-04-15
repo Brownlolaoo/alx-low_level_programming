9a32fe6088b98753cc56f3cc2d1c8ac57a9f2136
Brownlola <aramyedeey199@yahoo.com>
2022-04-15 04:31:34 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints int with putchar
+ *@n: takes number, hello
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+unsigned int n1;
+
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
+
+if(n1 / 10)
+{
+print_number(n1 / 10);
+}
+
+_putchar((n1 % 10) + '0');
+}
+
+
+
+
