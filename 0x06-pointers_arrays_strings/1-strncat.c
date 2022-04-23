d66705329af715d63172bc3e97261528bdf60907
Brownlola <aramyedeey199@yahoo.com>
2022-04-23 03:47:18 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _strncat - concatenates two strings
+ *_strncat - concatenates two strings
  *@dest: A pointer to a character that will be changed
- *@n: A pointer to a character that will be changed
+ *@n: value
  *@src: A pointer to a character that will be changed
- * Return: dest
+ *Return: dest
  */
 
-char * _strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 int i, u;
 
@@ -16,6 +16,7 @@ while (dest[i] != '\0')
 {
 i++;
 }
+
 u = 0;
 while (j < n && src[u] != '\0')
 {
@@ -23,7 +24,9 @@ dest[i] = src[j];
 u++;
 i++;
 }
+
 dest[i] = '\0';
+
 return (dest);
 }
 
