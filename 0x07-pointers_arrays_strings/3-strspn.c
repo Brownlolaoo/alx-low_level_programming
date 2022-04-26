b4e6addd104499ea367971f4807977a934c2c62b
Brownlola <aramyedeey199@yahoo.com>
2022-04-26 02:53:46 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @accept: This is thr string contain the list of characters to match in s
  * Return: return count
  **/
-unsigned int _strtnp(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
 	int count = 0;
