957dd3c8d402b2133eef09fecb2e124597c0b1fd
Brownlola <aramyedeey199@yahoo.com>
2022-04-26 06:56:26 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,9 +2,9 @@
 /**
  * print_chessboard - print the chessboard, 2d array
  * @a: 2d array of chars
- * Return: void
+ *Return: void
  */
-void print_chessboard(char (a)[8])
+void print_chessboard(char (*a)[8])
 {
 /*Declaring variables*/
 int i, u;
