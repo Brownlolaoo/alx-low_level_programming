60c0f52c6cd9ae2b5bc6a67970e1ffb456952366
Brownlola <aramyedeey199@yahoo.com>
2022-05-02 08:16:10 -0700

    task four

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@
  * check_num - check - string there are digit
  * @str: array str
  *
- * Return; Always 0 (success)
+ * Return: Always 0 (success)
  */
 int check_num(char *str)
 {
@@ -45,7 +45,7 @@ if(check_num(argv[count]))
 str_to_int + atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
-/*condition if one of the numbercontains symbols that are not digits*/
+/*condition if one of the number contains symbols that are not digits*/
 else
 {
 printf("Error\n");
