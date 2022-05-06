fbbe077a23beca18a3726f72fd624d2a7cab7ebb
Brownlola <aramyedeey199@yahoo.com>
2022-05-06 07:37:03 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,11 +17,11 @@ for (i = 1; i < argc; i++)
 	for (u = 0; argv[i][u] != '\0'; u++)
 {
 	if (argv[i][u] > 57 || argv[i][u] < 48)
-	{ printf("Error\n");
-		exit(98); }
+	{  printf("Error\n");
+        exit(98); }
 }
 }
-mul = atoi(argv[1]) *atoi(argv[2]);
+mul = atoI(argv[1]) *atoI(argv[2]);
 printf("%lu\n", mul);
 return (0);
 }
