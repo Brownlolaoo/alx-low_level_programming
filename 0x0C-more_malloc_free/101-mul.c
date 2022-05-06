405b6bfc64be9131bc229363aa81eea7a8c74d35
Brownlola <aramyedeey199@yahoo.com>
2022-05-06 06:59:50 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * main - multiplies two positive numbers
  * @argc: n arguments
  * @argv: args
  * Return: int
@@ -10,14 +11,14 @@ unsigned long mul;
 int i, u;
 if (argc != 3)
 { printf("Error\n");
-exit(98);}
+exit(98); }
 for (i = 1; i < argc; i++)
 {
-for (u = 0; argv[i][u] != '\0'; u++)
+	for (u = 0; argv[i][u] != '\0'; u++)
 {
-if (argv[i][u] > 57 || argv[i][u] < 48)
-{ printf("Error\n");
-exit(98); }
+	if (argv[i][u] > 57 || argv[i][u] < 48)
+	{ printf("Error\n");
+		exit(98); }
 }
 }
 mul = atoi(argv[1]) *atoi(argv[2]);
