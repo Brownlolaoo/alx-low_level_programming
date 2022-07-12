3240cd5928478694cc8f4ca91063990c83aed6d9
Brownlola <aramyedeey199@yahoo.com>
2022-07-11 22:57:12 -0700

    files

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, u;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+u = 0;
+while (u < n && src[u] != '\0')
+{
+dest[i] = src[u];
+u++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
