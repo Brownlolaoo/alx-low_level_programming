bc909733c1e3450fc0dc54ee1b1e027ce68c0f54
Brownlola <aramyedeey199@yahoo.com>
2022-07-11 21:48:32 -0700

    files

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - evaluate if letter is uppercase
+ *@c: print int
+ * Return: give 1 or 2
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
